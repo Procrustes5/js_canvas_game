ad51b4c43d05ceedde6d8d5fcca03b7a97245395
Hanse Kim <procrustes5@gmail.com>
2023-01-23 21:21:08 +0900

    230123 Player Added
    Visualizing Map
    Centering Player Image
    Player Moving System

--- main.js
+++ main.js
@@ -1,25 +1,97 @@
 // Font
+const CHRHEIGHT = 9;     // Size of Character
+const CHRWIDTH = 8;
 const FONT = "48px monospace";
-const HEIGHT = 120;  // Size of Virtual Screen
+const HEIGHT = 120;      // Size of Virtual Screen
 const WIDTH = 128;
-const SMOOTH = 0;    // Retouching
+const MAP_WIDTH = 32;    // Size of Map
+const MAP_HEIGHT = 32;
+const SMOOTH = 0;        // Retouching
+const TILECOLUMN = 4     // Tile column
+const TILEROW = 4        // Tile row
+const TILESIZE = 8;      // Tile Size 
 
-let gScreen = 0;     // Virtual Screen
-let gFrame = 0;      // Internal Counter
-let gImgMap;         // Map Image
-let gWidth;          // Size of window
+
+let gScreen = 0;         // Virtual Screen
+let gFrame = 0;          // Internal Counter
+let gImgMap;             // Map Image
+let gImgPlayer;          // Player Image
+let gWidth;              // Size of window
 let gHeight;
+let gPlayerX = 0;        // Position of Player
+let gPlayerY = 0;
+
+const gFileMap = "img/map.png";
+const gFilePlayer = "img/player.png"
+
+// Map
+//  1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
+const gMap = [
+    0, 0, 0, 0, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 1
+    0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 2
+    0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 3
+    0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 4
+    0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 5
+    0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 6
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 7
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 8
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 9
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 10
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 11
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 12
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 13
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 14
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 15
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 16
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 17
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 18
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 19
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 20
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 21
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 22
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 23
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 24
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 25
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 26
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 27
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 28
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 29
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 30
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 31
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 4, 4, 4   // 32
+]
 
 function DrawMain() {
     // get 2d drawing context of Virtual Screen
     const g = gScreen.getContext("2d");  
-    for( let y = 0; y < 32; y++) {
-        for( let x = 0; x < 64; x ++){
-            g.drawImage( gImgMap, x*32, y*32);
+    for( let y = 0; y < 20; y++) {
+        for( let x = 0; x < 20; x ++){
+
+            let px = gPlayerX + x;
+            let py = gPlayerY + y;
+            DrawTile( g, x*TILESIZE, y*TILESIZE, gMap[ py * MAP_WIDTH + px]);
+            
         }
     }
-    g.font = FONT;                              // Set Basic Font
-    g.fillText("Hello World " + gFrame, gFrame/10, 64);
+    g.drawImage( gImgPlayer, 
+        0, 0, CHRWIDTH, CHRHEIGHT, 
+        WIDTH/2, HEIGHT/2, CHRWIDTH, CHRHEIGHT);
+
+    // g.font = FONT;                              // Set Basic Font
+    // g.fillText("Hello World " + gFrame, gFrame/10, 64);
+}
+
+function DrawTile (g, x, y, idx) {
+    const ix = (idx % TILECOLUMN) * TILESIZE;
+    const iy = Math.floor(idx / TILECOLUMN) * TILESIZE;
+    g.drawImage( gImgMap, ix, iy, TILESIZE, TILESIZE, x, y, TILESIZE, TILESIZE);
+}
+
+function LoadImage() {
+    gImgMap = new Image();
+    gImgMap.src = gFileMap;                // Read Map source
+    gImgPlayer = new Image();
+    gImgPlayer.src = gFilePlayer;          // Read Player source
 }
 
 function WmPaint() {
@@ -57,10 +129,20 @@ function WmTimer() {
     WmPaint();
 }
 
+// Key Down Event
+window.onkeydown = function( ev ) {
+    let c = ev.keyCode;     // Get Key code
+
+    if (c == 37) gPlayerX--; // Left
+    if (c == 38) gPlayerY--; // Up
+    if (c == 39) gPlayerX++; // Right
+    if (c == 40) gPlayerY++; // Down
+}
+
 // browser start event
 window.onload = function() {
-    gImgMap = new Image();
-    gImgMap.src = "img/map.png";                // Read Map source
+    LoadImage();
+    
     gScreen = document.createElement("canvas"); // Create Virtual Screen
     // Set Size of Virtual Screen
     gScreen.width = WIDTH; 
